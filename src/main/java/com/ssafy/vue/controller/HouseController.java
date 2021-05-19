d6fe3f5f57045f216a1733d7e935a5a1b8623bf7
yallyyally <jhshin0028@gmail.com>
2021-05-19 23:27:20 +0900

    initial commit

---
+++ src/main/java/com/ssafy/vue/controller/HouseController.java
@@ -0,0 +1,21 @@
+package com.ssafy.vue.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/house")
+public class HouseController {
+	
+	@GetMapping
+	public void getConnection() {
+		System.out.println("house로 연결요청 스 ~~~~~~");
+		System.out.println("무야호 ");
+	}
+	
+//	매물 요청
+//	매물 검색
+	//test
+	
+}
