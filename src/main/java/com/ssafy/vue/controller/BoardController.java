1f5b32a231e28d1657d9c037399eb2f908e8872c
노효진 <23gywls@naver.com>
2021-05-20 20:00:08 +0900

    댓글기능 기초작업

--- src/main/java/com/ssafy/vue/controller/BoardController.java
+++ src/main/java/com/ssafy/vue/controller/BoardController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.vue.dto.BoardDto;
+import com.ssafy.vue.dto.CommentDto;
 import com.ssafy.vue.service.BoardService;
 
 import io.swagger.annotations.ApiOperation;
@@ -85,6 +86,38 @@ public class BoardController {
 	public ResponseEntity<String> deleteBoard(@PathVariable int no) {
 		logger.debug("deleteBoard - 호출");
 		if (boardService.deleteBoard(no)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+    
+    @ApiOperation(value = "새로운 댓글을 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PostMapping(value="/detail/{no}")
+	public ResponseEntity<String> writeComment(@RequestBody CommentDto comment) {
+		logger.debug("writeComment - 호출");
+		if (boardService.writeComment(comment)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+    @ApiOperation(value = "댓글번호에 해당하는 댓글의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PutMapping("/update/{no}")
+	public ResponseEntity<String> updateComment(@RequestBody CommentDto comment) {
+		logger.debug("updateComment - 호출");
+		logger.debug("" + comment);
+		
+		if (boardService.updateComment(comment)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+    @ApiOperation(value = "댓글번호에 해당하는 댓글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@DeleteMapping("/delete/{no}")
+	public ResponseEntity<String> deleteComment(@PathVariable int no) {
+		logger.debug("deleteComment - 호출");
+		if (boardService.deleteComment(no)) {
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
---
+++ src/main/java/com/ssafy/vue/dao/CommentMapper.java
@@ -0,0 +1,17 @@
+package com.ssafy.vue.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.vue.dto.CommentDto;
+
+@Mapper
+public interface CommentMapper {
+	
+	public List<CommentDto> cmtList(int bno);
+	public int insertComment(CommentDto comment);
+	public int updateComment(CommentDto comment);
+	public int deleteComment(int no);
+	
+}
